d6fdf598f58080bf1cda2c3e23d493e77c018501
Greg <gregoire@saugnac.me>
2023-11-15 09:06:15 +0000

    Bug Fixes
    Fixed:
    - resizing buttons (+/-) did not scale grid properly
    - ability to start without start/finish cells being placed
    - ability to press buttons during algorithm runtime
    
    Bugs:
    - grid resizing leaves blanks on edges for certain cell sizes
    - can draw on grid during runtime
    - drawing out of bounds leads to a crash

--- main.py
+++ main.py
@@ -34,30 +34,8 @@ CELL_GAP = 25
 COLS = int(GRID_WIDTH//CELL_GAP)
 ROWS = int(GRID_HEIGHT//CELL_GAP)
 
-class cell:
-    def __init__(self,row,col,width,colour=BLANK_COLOUR):
-        self.row, self.col = row, col
-        self.width = width
-        self.x = self.col*self.width
-        self.y = self.row*self.width
-        self.neighbours = []
-        self.colour = colour
-        self.pathfind_prior = None
 
-    def draw(self,win):
-        pygame.draw.rect(win,self.colour,(self.x,self.y,self.width,self.width))
-    def update_neighbours(self,rows,cols):
-        self.neighbours = []
-        if self.row > 0:
-            self.neighbours.append(grid[self.row-1][self.col])
-        if self.row < rows-1:
-            self.neighbours.append(grid[self.row+1][self.col])
-        if self.col > 0:
-            self.neighbours.append(grid[self.row][self.col-1])
-        if self.col < cols-1:
-            self.neighbours.append(grid[self.row][self.col+1])
-
-def dijkstra(start_cell,fin_cell):
+def dijkstra(start_cell,fin_cell,grid):
     global win, WIDTH, HEIGHT, GRID_WIDTH, GRID_HEIGHT
     Q = []
     Q.append(start_cell)
@@ -116,9 +94,9 @@ def dijkstra(start_cell,fin_cell):
             curr_cell.colour = PATH_COLOUR
             curr_cell = curr_cell.pathfind_prior
             pathfPage.load()
-            sleep(.01)
+            #sleep(.01)
 
-def greedybfs(start_cell,fin_cell):
+def greedybfs(start_cell,fin_cell,grid):
     visited = []
     open = []
     curr_cell = start_cell
@@ -156,7 +134,7 @@ def greedybfs(start_cell,fin_cell):
         if curr_cell.colour != START_COLOUR: curr_cell.colour = VISITED_COLOUR
         
         start_cell.colour = START_COLOUR
-        draw_surface()
+        pathfPage.load()
         
     for row in grid:
         for n in row:
@@ -174,7 +152,7 @@ def greedybfs(start_cell,fin_cell):
             if curr_cell == start_cell: break
             curr_cell.colour = PATH_COLOUR
             curr_cell = curr_cell.pathfind_prior
-            draw_surface()
+            pathfPage.load()
             sleep(.1)
             
         
@@ -240,7 +218,7 @@ def aStar(start_cell,fin_cell):
         curr_cell_idx = openFcosts.index(min(openFcosts))
         curr_cell = open[curr_cell_idx][0]
         
-        draw_surface()
+        pathfPage.load()
     
     if found:
         pass
@@ -250,219 +228,8 @@ def aStar(start_cell,fin_cell):
         #     if curr_cell.colour != FIN_COLOUR:
         #         curr_cell.colour = PATH_COLOUR
         #     curr_cell = curr_cell.pathfind_prior
-            
-grid = []
-def init_grid(): 
-    global grid
-    COLS = int(GRID_WIDTH//CELL_GAP)
-    ROWS = int(GRID_HEIGHT//CELL_GAP)
-    grid = []
-
-    for i in range(ROWS):
-        grid.append([])
-        for j in range(COLS):
-            if i == 0 or j == 0 or i == ROWS-1 or j == COLS-1:
-                grid[i].append(cell(i,j,CELL_GAP,colour=BORDER_COLOUR))
-            else:
-                grid[i].append(cell(i,j,CELL_GAP))
-
-    for row in grid:
-        for n in row: n.update_neighbours(ROWS,COLS)
-init_grid()
-
-def draw_grid(rows,cols):
-    pass
-    # for i in range(cols):
-    #     pygame.draw.line(win,GRID_COLOUR,(i*CELL_GAP,0),(i*CELL_GAP,GRID_HEIGHT))
-    # for j in range(rows):
-    #     pygame.draw.line(win,GRID_COLOUR,(0,j*CELL_GAP),(GRID_WIDTH,j*CELL_GAP))
-
-selected_algo = 'dijstra'
-
-def select_algo(algo):
-    global selected_algo
-    selected_algo = algo
-
-# UI ELEMENTS:
-
-buttons = []
-
-def randomNodes():
-    for row in grid:
-        for n in row:
-            if (n.colour == BLANK_COLOUR or n.colour == PATH_COLOUR) and random.randint(1,15) == 1:
-                n.colour = BARRIER_COLOUR
-
-def increaseGridSize():
-    global CELL_GAP
-    CELL_GAP += 5
-    init_grid()
-    draw_grid(ROWS,COLS)
-    
-def decreaseGridSize():
-    global CELL_GAP
-    if CELL_GAP > 5:
-        CELL_GAP -= 5
-        ROWS = int(GRID_HEIGHT//CELL_GAP)
-        COLS = int(GRID_WIDTH//CELL_GAP)
-        init_grid()
-        draw_grid(ROWS,COLS)
-
-randomButton = basicUI.button(win,"random",lambda:randomNodes(),(GRID_WIDTH+(WIDTH-GRID_WIDTH)//2-60,100),
-                              fg=UI_TEXT_COLOUR,bg=GRID_COLOUR,group=buttons)
-
-largerGrid = basicUI.button(win,"+",lambda:increaseGridSize(),(GRID_WIDTH+(WIDTH-GRID_WIDTH)//2-60,250),
-                            fg=UI_TEXT_COLOUR,bg=GRID_COLOUR,group=buttons)
-smallerGrid = basicUI.button(win,"-",lambda:decreaseGridSize(),(GRID_WIDTH+(WIDTH-GRID_WIDTH)//2,250),
-                             fg=UI_TEXT_COLOUR,bg=GRID_COLOUR,group=buttons)
-
-def resetGrid():
-    for i,row in enumerate(grid):
-        for j in range(len(row)):
-            if i != 0 and i != ROWS-1 and j != 0 and j != COLS-1:
-                grid[i][j].colour = BLANK_COLOUR
-resetButton = basicUI.button(win,"reset",lambda:resetGrid(),(GRID_WIDTH+(WIDTH-GRID_WIDTH)//2-60,150),
-                             fg=UI_TEXT_COLOUR,bg=GRID_COLOUR,group=buttons)
-started = False
-def startAlgo():
-    started = True
-startButton = basicUI.button(win,"start",lambda:startAlgo(),(GRID_WIDTH+(WIDTH-GRID_WIDTH)//2-60,200),
-                             fg=UI_TEXT_COLOUR,bg=GRID_COLOUR,group=buttons)
-
-algoChoice = basicUI.dropdown(win,"Algorithm",(GRID_WIDTH+(WIDTH-GRID_WIDTH)//2-60,300),fg=UI_TEXT_COLOUR,bg=GRID_COLOUR)
-algoChoice.add_option("dijkstra",lambda:select_algo("dijkstra"))
-algoChoice.add_option("A star",lambda:select_algo("astar"))
-algoChoice.add_option("greedy BFS",lambda:select_algo("greedybfs"))
-algoChoice.add_option("dynamic",lambda:select_algo("dynamic"))
-
-def draw_surface():
-    win.fill((255,255,255))
-    for row in grid:
-        for cell in row: cell.draw(win)
-    draw_grid(ROWS,COLS)
-    yMargin = 10
-    pygame.draw.rect(win,UI_MAIN_COLOUR,(GRID_WIDTH,0,UI_WIDTH,HEIGHT))
-    basicUI.text(win,"Pathfinding",GRID_WIDTH+(WIDTH-GRID_WIDTH)//2-75,yMargin,UI_TEXT_COLOUR,45)
-    for button in buttons:
-        button.update()
-        button.draw()
-    algoChoice.update()
-
-    pygame.display.update()
-
-def check_grid(condition):
-    for row in grid:
-        for cell in row:
-            if cell.colour == condition:
-                return True
-    return False
-
-def in_bounds(x,y):
-    if pygame.mouse.get_pos()[0] > GRID_WIDTH:
-        return False
-    if x == 0 or y == 0 or x == ROWS-1 or y == COLS-1:
-        return False
-    return True
 
 
-# def menu():
-#     global win, WIDTH, HEIGHT, GRID_WIDTH, GRID_HEIGHT
-#     running = True
-#     start_button = basicUI.button(win,"START",lambda:main(),(0,0),fontsize=60,fg=UI_TEXT_COLOUR,bg=GRID_COLOUR)
-#     start_button.new_center((WIDTH//2,HEIGHT//2-start_button.fontsize//1.5))
-#     quit_button = basicUI.button(win,"QUIT",lambda:quit_func(),(0,0),fontsize=55,fg=UI_TEXT_COLOUR,bg=GRID_COLOUR)
-#     quit_button.new_center((WIDTH//2,HEIGHT//2+quit_button.fontsize//1.5))
-#     docs_button = basicUI.button(win,"DOCS",lambda:open_docs(),(0,0),fontsize=20,fg=UI_TEXT_COLOUR,bg=GRID_COLOUR)
-#     margin = 10
-#     docs_button.new_center((WIDTH-docs_button.bgRect.width//2-margin,margin+docs_button.fontsize//1.5))
-#     while running:
-#         CLOCK.tick(FPS)
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 quit()
-#             if event.type == pygame.VIDEORESIZE:
-#                 print("resizing")
-#                 old_win = win
-#                 WIDTH,HEIGHT = event.w,event.h
-#                 GRID_WIDTH, GRID_HEIGHT = WIDTH*0.75, HEIGHT
-#                 win = pygame.display.set_mode((WIDTH,HEIGHT),pygame.RESIZABLE)
-#                 win = old_win
-#                 del old_win
-                
-#         win.fill(BLANK_COLOUR)
-#         start_button.update()
-#         quit_button.update()
-#         docs_button.update()
-#         start_button.draw()
-#         quit_button.draw()
-#         docs_button.draw()
-#         pygame.display.update()
-
-def open_docs():
-    webbrowser.open("https://docs.google.com/document/d/1U96vEvA0jDv8XYHGnGShQUluY2gDUlA39RXWsx02tb4/edit")
-
-def quit_func():
-    pygame.quit()
-    quit()
-
-def main():
-    global win, WIDTH, HEIGHT, GRID_WIDTH, GRID_HEIGHT, started
-    running = True
-    start_cell,fin_cell = None,None
-    while running:
-        CLOCK.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.VIDEORESIZE:
-                old_win = win
-                WIDTH,HEIGHT = event.w,event.h
-                GRID_WIDTH, GRID_HEIGHT = WIDTH*0.75, HEIGHT
-                win = pygame.display.set_mode((WIDTH,HEIGHT),pygame.RESIZABLE)
-                win = old_win
-                del old_win
-                
-        if pygame.mouse.get_pressed():
-            mouse_pos = pygame.mouse.get_pos()
-            x_coord = mouse_pos[1] // CELL_GAP
-            y_coord = mouse_pos[0] // CELL_GAP
-            if pygame.mouse.get_pressed()[0] and in_bounds(x_coord,y_coord):
-                if not check_grid(START_COLOUR):
-                    grid[x_coord][y_coord].colour = START_COLOUR    
-                    start_cell = grid[x_coord][y_coord]
-                elif not check_grid(FIN_COLOUR) and check_grid(START_COLOUR) and grid[x_coord][y_coord].colour != START_COLOUR:
-                    grid[x_coord][y_coord].colour = FIN_COLOUR
-                    fin_cell = grid[x_coord][y_coord]
-                elif grid[x_coord][y_coord].colour != START_COLOUR and grid[x_coord][y_coord].colour != FIN_COLOUR:
-                    grid[x_coord][y_coord].colour = BARRIER_COLOUR
-            if pygame.mouse.get_pressed()[2] and in_bounds(x_coord,y_coord) and grid[x_coord][y_coord].colour:
-                grid[x_coord][y_coord].colour = BLANK_COLOUR
-                
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_ESCAPE]:
-            menu()
-        
-        if (keys_pressed[pygame.K_SPACE] and start_cell != None and fin_cell != None) or started:
-            for i,row in enumerate(grid):
-                for j in range(len(row)):
-                    c = grid[i][j].colour
-                    if c==VISITED_COLOUR or c==QUEUED_COLOUR or c==PATH_COLOUR:
-                        grid[i][j].colour = BLANK_COLOUR
-            dijkstra(start_cell,fin_cell)
-            started = False
-        if keys_pressed[pygame.K_r]:
-            resetGrid()
-        if keys_pressed[pygame.K_a] and start_cell != None and fin_cell != None:
-            for i,row in enumerate(grid):
-                for j in range(len(row)):
-                    c = grid[i][j].colour
-                    if c==VISITED_COLOUR or c==QUEUED_COLOUR or c==PATH_COLOUR:
-                        grid[i][j].colour = BLANK_COLOUR
-            aStar(start_cell,fin_cell)
-        draw_surface()
-    pygame.quit()
-    quit()
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
@@ -489,7 +256,7 @@ def event_handler():
             del prev_win
 
 menuPage = menu.Menu(win,WIDTH,HEIGHT,lambda:toPathfinding())
-pathfPage = pathfinding.Pathfinding(win,WIDTH,HEIGHT,lambda:toMenu(),grid)
+pathfPage = pathfinding.Pathfinding(win,WIDTH,HEIGHT,lambda:toMenu())
 
 if __name__ == '__main__':
     running = True
@@ -508,15 +275,17 @@ if __name__ == '__main__':
                         for j in range(len(row)):
                             c = pathfPage.grid[i][j].colour
                             if c==VISITED_COLOUR or c==QUEUED_COLOUR or c==PATH_COLOUR:
-                                grid[i][j].colour = BLANK_COLOUR
+                                pathfPage.grid[i][j].colour = BLANK_COLOUR
                     if pathfPage.selected_algo == 'dijkstra':
-                        dijkstra(pathfPage.start_cell,pathfPage.fin_cell)
+                        dijkstra(pathfPage.start_cell,pathfPage.fin_cell,pathfPage.grid)
                     elif pathfPage.selected_algo == 'astar':
                         print("astar")
                     elif pathfPage.selected_algo == 'greedybfs':
-                        greedybfs(pathfPage.start_cell,pathfPage.fin_cell)
+                        greedybfs(pathfPage.start_cell,pathfPage.fin_cell,pathfPage.grid)
                     elif pathfPage.selected_algo == "dynamic":
                         print("dynamic")
+                    pathfPage.start_search = False
+                else:
                     pathfPage.start_search = False
         pygame.display.update()
         
--- pathfinding.py
+++ pathfinding.py
@@ -18,19 +18,44 @@ UI_MAIN_COLOUR = (30,30,30)
 UI_BG_COLOUR = (50,50,50)
 UI_TEXT_COLOUR = (100,100,100)
 
+class cell:
+    def __init__(self,row,col,width,colour=BLANK_COLOUR):
+        self.row, self.col = row, col
+        self.width = width
+        self.x = self.col*self.width
+        self.y = self.row*self.width
+        self.neighbours = []
+        self.colour = colour
+        self.pathfind_prior = None
+
+    def draw(self,win):
+        pygame.draw.rect(win,self.colour,(self.x,self.y,self.width,self.width))
+    def update_neighbours(self,rows,cols,grid):
+        self.neighbours = []
+        if self.row > 0:
+            self.neighbours.append(grid[self.row-1][self.col])
+        if self.row < rows-1:
+            self.neighbours.append(grid[self.row+1][self.col])
+        if self.col > 0:
+            self.neighbours.append(grid[self.row][self.col-1])
+        if self.col < cols-1:
+            self.neighbours.append(grid[self.row][self.col+1])
+
 class Pathfinding:
-    def __init__(self,win,width,height,menu_func,grid):
+    def __init__(self,win,width,height,menu_func):
         self.win = win
         self.width, self.height = width, height
         self.grid_width, self.grid_height = 0.75*width, height
         self.ui_width = self.width-self.grid_width
         self.menu_func = menu_func
-        self.grid = grid
+        self.grid = []
         self.cellsize = 25
         self.rows = int(self.grid_height//self.cellsize)
         self.cols = int(self.grid_width//self.cellsize)
-        self.show_lines = False
+        self.show_lines = True
         self.buttons = []
+        
+        self.init_grid()
         
         self.start_search = False
         self.selected_algo = 'dijkstra'
@@ -56,14 +81,36 @@ class Pathfinding:
         self.gridLinesButton = basicUI.button(self.win,"show lines", lambda:self.showLinesFunc(),
                                               (self.grid_width+(self.width-self.grid_width)//2-60,300),
                                               fg=UI_TEXT_COLOUR,bg=UI_BG_COLOUR,group=self.buttons)
-        self.algoSelector = basicUI.dropdown(self.win,"algorithm",
+        self.bwerdsYTButton = basicUI.button(self.win,"subscribe",lambda:self.bwerdsLinkFunc(),
                                              (self.grid_width+(self.width-self.grid_width)//2-60,350),
+                                             fg=UI_TEXT_COLOUR,bg=UI_BG_COLOUR,group=self.buttons)
+        self.algoSelector = basicUI.dropdown(self.win,"algorithm",
+                                             (self.grid_width+(self.width-self.grid_width)//2-60,400),
                                              fg=UI_TEXT_COLOUR,bg=UI_BG_COLOUR)
         self.algoSelector.add_option("dijkstra",lambda:self.select_algo("dijkstra"))
         self.algoSelector.add_option("A star",lambda:self.select_algo("astar"))
         self.algoSelector.add_option("greedy BFS",lambda:self.select_algo("greedybfs"))
         self.algoSelector.add_option("dynamic",lambda:self.select_algo("dynamic"))
+    
+    def init_grid(self):
+        self.grid = []
+        self.cols = int(self.grid_width//self.cellsize)
+        self.rows = int(self.grid_height//self.cellsize)
         
+        for i in range(self.rows):
+            self.grid.append([])
+            for j in range(self.cols):
+                if i == 0 or j == 0 or i == self.rows-1 or j == self.cols-1:
+                    self.grid[i].append(cell(i,j,self.cellsize,colour=BORDER_COLOUR))
+                else:
+                    self.grid[i].append(cell(i,j,self.cellsize))
+        for row in self.grid:
+            for n in row:
+                n.update_neighbours(self.rows,self.cols,self.grid)
+    
+    def bwerdsLinkFunc(self):
+        webbrowser.open("https://www.youtube.com/@bwerds1263")
+    
     def randomFunc(self):
         if not self.start_search:
             for row in self.grid:
@@ -79,15 +126,19 @@ class Pathfinding:
     def startFunc(self):
         self.start_search = True
     def largerGridFunc(self):
-        if not self.start_search:
+        if not self.start_search and self.cellsize + 5 < min((self.grid_width,self.grid_height)):
             self.cellsize += 5
+            self.cols = int(self.grid_width//self.cellsize)
+            self.rows = int(self.grid_height//self.cellsize)
             self.resetFunc()
-            self.load()
+            self.init_grid()
     def smallerGridFunc(self):
-        if not self.start_search:
+        if not self.start_search and self.cellsize -5 > 0:
             self.cellsize -= 5
+            self.cols = int(self.grid_width//self.cellsize)
+            self.rows = int(self.grid_height//self.cellsize)
             self.resetFunc()
-            self.load()
+            self.init_grid()
     def showLinesFunc(self):
         self.show_lines = not self.show_lines
     def select_algo(self,algorithm):
@@ -103,10 +154,30 @@ class Pathfinding:
         for row in self.grid:
             for n in row:
                 if n.colour == colour:
-                    return True
-        return False
+                    return n
+
+    
+    def draw_grid(self):
+        for row in self.grid:
+            for cell in row: cell.draw(self.win)
+        if self.show_lines:
+            for i in range(self.cols):
+                pygame.draw.line(self.win,GRID_COLOUR,(i*self.cellsize,0),(i*self.cellsize,self.grid_height))
+            for j in range(self.rows):
+                pygame.draw.line(self.win,GRID_COLOUR,(0,j*self.cellsize),(self.grid_width,j*self.cellsize))
     
     def load(self):
+        
+        isStartCell = self.check_grid(START_COLOUR)
+        isFinCell = self.check_grid(FIN_COLOUR)
+        if not isStartCell: 
+            self.start_cell = None
+        else:
+            self.start_cell = isStartCell
+        if not isFinCell:
+            self.fin_cell = None
+        else:
+            self.fin_cell = isFinCell
         
         # CHECK KEYS PRESSED
         keys = pygame.key.get_pressed()
@@ -139,21 +210,16 @@ class Pathfinding:
             
             # DRAW WINDOW
             self.win.fill((255,255,255))
-            for row in self.grid:
-                for cell in row: cell.draw(self.win)
             
-            if self.show_lines:
-                for i in range(self.cols):
-                    pygame.draw.line(self.win,GRID_COLOUR,(i*self.cellsize,0),(i*self.cellsize,self.grid_height))
-                for j in range(self.rows):
-                    pygame.draw.line(self.win,GRID_COLOUR,(0,j*self.cellsize),(self.grid_width,j*self.cellsize))
+            self.draw_grid()
             
             yMargin = 10
             pygame.draw.rect(self.win,UI_MAIN_COLOUR,(self.grid_width,0,self.ui_width,self.height))
             basicUI.text(self.win,"Pathfinding",self.grid_width+(self.width-self.grid_width)//2-75,
                          yMargin,UI_TEXT_COLOUR,45)
             for button in self.buttons:
-                button.update()
+                if not self.start_search or button == self.gridLinesButton:
+                    button.update()
                 button.draw()
             self.algoSelector.update()
             
